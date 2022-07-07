f3d1c14c5697e800540d15c44012b303fe5820b9
echio <22025340@qq.com>
2022-07-07 11:10:43 +0800

    4th

---
+++ build/src/CMakeFiles/RCS.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "../../bin/RCS"
+  "../../bin/RCS.pdb"
+  "CMakeFiles/RCS.dir/chatinfo.cpp.o"
+  "CMakeFiles/RCS.dir/chatinfo.cpp.o.d"
+  "CMakeFiles/RCS.dir/db.cpp.o"
+  "CMakeFiles/RCS.dir/db.cpp.o.d"
+  "CMakeFiles/RCS.dir/main.cpp.o"
+  "CMakeFiles/RCS.dir/main.cpp.o.d"
+  "CMakeFiles/RCS.dir/server.cpp.o"
+  "CMakeFiles/RCS.dir/server.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RCS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
